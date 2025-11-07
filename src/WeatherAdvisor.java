2207d54d18f790c1b9cb8c2396b70f384a401d45
Kevin Andres Zapata Murillo <kevinzapatajega@gmail.com>
2025-11-07 11:59:04 -0500

    Desafio FizzBuzz mejora

--- src/WeatherAdvisor.java
+++ src/WeatherAdvisor.java
@@ -1,16 +1,41 @@
+import java.util.Scanner;
+
 public class WeatherAdvisor {
     public static void main(String[] args) {
-        // FizzBuzz básico: del 1 al 100
-        for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el número inicial: ");
+        int start = scanner.nextInt();
+
+        System.out.print("Ingrese el número final: ");
+        int end = scanner.nextInt();
+
+        if (start <= 0 || end <= 0 || start >= end) {
+            System.out.println("Rango inválido. Se usará el rango por defecto (1 a 200).");
+            start = 1;
+            end = 200;
+        }
+
+        for (int i = start; i <= end; i++) {
+            boolean fizz = i % 3 == 0;
+            boolean buzz = i % 5 == 0;
+            boolean boom = i % 7 == 0;
+
+            if (fizz && buzz && boom) {
+                System.out.println("FizzBuzzBoom");
+            } else if (fizz && buzz) {
                 System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
+            } else if (fizz) {
                 System.out.println("Fizz");
-            } else if (i % 5 == 0) {
+            } else if (buzz) {
                 System.out.println("Buzz");
+            } else if (boom) {
+                System.out.println("Boom");
             } else {
                 System.out.println(i);
             }
         }
+
+        scanner.close();
     }
 }
